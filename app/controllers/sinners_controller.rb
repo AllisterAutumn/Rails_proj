47127d146e2d48f5ce5bba7c8faa8530003a350e
val rigodon <rvrigodon@gmail.com>
2017-02-19 22:55:58 -0500

    users can add, show and render sins

--- app/controllers/sinners_controller.rb
+++ app/controllers/sinners_controller.rb
@@ -1,9 +1,28 @@
 class SinnersController < ApplicationController
+/this route is to display all the sinners and their sins/
+def index
+  @sinners = Sinner.all
+end
+/this route is to show specific confessions. It uses the find method to retrieve the sin by id/
+def show
+  @sinner = Sinner.find(params[:id])
+end
+
   /my route to to the new sinners form/
   def new
   end
-  /my post route for the sinners form/
-  def create
-    render plain: params[:sinner].inspect
+
+  /my post route for the sinners form. It takes the params and redirects back to the main page/
+def create
+  @sinner = Sinner.new(sinner_params)
+  @sinner.save
+  redirect_to @sinner
+end
+
+/this extra route is for security measures like 'permit' to protect my app from malicious activity/
+private
+  def sinner_params
+    params.require(:sinner).permit(:name, :sin)
   end
 end
+
